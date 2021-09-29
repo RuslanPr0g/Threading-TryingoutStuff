ce4face567c565daaa39aa1772670224999b6de6
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-09-29 13:05:10 +0300

    initial commit

---
+++ ThreadingTryingoutStuff/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingTryingoutStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sync execution of functions:");
+            Function1();
+            Function2();
+
+            Console.WriteLine("\nParallel execution of functions:");
+            Thread obj1 = new(Function1);
+            Thread obj2 = new(Function2);
+
+            obj1.Start();
+            obj2.Start();
+        }
+
+        static void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 1: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Function2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 2: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
