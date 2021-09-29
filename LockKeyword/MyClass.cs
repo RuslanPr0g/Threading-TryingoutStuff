821de4e1d91e778ecf6b5080c57c4cf3a7c3504b
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-09-29 15:55:45 +0300

    Lock keyword

---
+++ LockKeyword/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace LockKeyword
+{
+    internal class MyClass
+    {
+        private readonly object _locker = new();
+
+        public void Method()
+        {
+            int hash = Thread.CurrentThread.GetHashCode();
+
+            lock (_locker)
+            {
+                for (int counter = 0; counter < 10; counter++)
+                {
+                    Console.WriteLine($"Thread # {hash}, iteration: {counter}");
+                    Console.Beep();
+                    Thread.Sleep(500);
+                }
+
+                Console.WriteLine(new string('-', 20));
+            }
+        }
+    }
+}
---
+++ LockKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace LockKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyClass instance = new();
+
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(instance.Method).Start();
+            }
+
+            Thread.Sleep(1000);
+
+            Console.ReadKey();
+        }
+    }
+}
